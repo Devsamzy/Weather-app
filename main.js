2027720085deaa0baa10a2149a7fa33271336c01
Aderogba Samuel <121461245+Devsamzy@users.noreply.github.com>
2025-06-04 06:16:22 +0100

    Uploar

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ async function weatherFn(cName) {
 }
 
 function weatherShowFn(data) {
-  	$('#city-name').text(data.name);
+  	$('#city-name').html(`${data.sys.country},${data.name}`);
 	  $('#date').text(moment().
 		format('ddd-DD-MM'));
 	  $('#temperature').
